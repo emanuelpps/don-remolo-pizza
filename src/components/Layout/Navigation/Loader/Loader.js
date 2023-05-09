547b139a3a199efb89eacd6e2a5655e90b2b92ea
Emanuel.ps <emanuelpages.ps@gmail.com>
2023-05-09 19:06:26 -0300

    pizza loader GIF

--- src/components/Layout/Navigation/Loader/Loader.js
+++ src/components/Layout/Navigation/Loader/Loader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Loader.css';
-
+import LoaderImg from '../../../../assets/images/pizza.gif'
 
 export default function Loader() {
   return (
     <div className='pizzaLoader'>
         <div className='pizzaBox text-center'>
-            <img className='pizzaLoaderImg' src={process.env.PUBLIC_URL + "/images/pizza.gif"} alt='Loading...'/>
+            <img className='pizzaLoaderImg' src={LoaderImg} alt='Loading...'/>
             <h4>Cargando...</h4>
         </div>
     </div>
